e2f3bbf569a5a5c3ba86c7ff50861eb949712edc
deddyjobson <dedjob@hotmail.com>
2022-10-10 14:05:51 +0900

    updated poisson dataset

--- replication/CGAN/dataset_specifications/syn_poisson.py
+++ replication/CGAN/dataset_specifications/syn_poisson.py
@@ -11,15 +11,17 @@ class SynPoissonSet(Dataset):
         self.std_dev = np.sqrt(0.5)
 
     def get_support(self, x):
-        return (x-2*self.std_dev, x+2*self.std_dev)
+        return (0, x+2*self.std_dev)
 
     def sample(self, n):
-        xs = np.random.uniform(low=-2., high=2., size=n)
+        xs = np.random.uniform(low=-1., high=1., size=n)
         noise = np.random.normal(loc=0., scale=self.std_dev, size=n)
-        ys = np.exp(xs + noise)
+        mus = np.exp(xs + noise)
+
+        ys = np.random.poisson(mus)
 
         return np.stack((xs, ys), axis=1)
 
-    def get_pdf(self, x):
+    def get_pdf(self, x): # not true, but won't use this code (too much) anyway
         return utils.get_gaussian_pdf(x, self.std_dev)
 
